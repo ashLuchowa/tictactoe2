73e671b06d03d960a4610eb7e95fceb57129f683
Ash Luchowa <ashwinluchowa@gmail.com>
2023-03-15 16:40:21 +0800

    Create primary gameStart Module

--- main.js
+++ main.js
@@ -2,3 +2,19 @@
 const players = (playerName, playerScore) => {
   return { playerName, playerScore };
 };
+
+//GameStart Module
+const gameStart = (() => {
+  let gameBoardContent = [
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""],
+  ];
+  const player1 = players("X", 0);
+  const player2 = players("O", 0);
+  let gameRound = 1;
+
+  return { gameBoardContent, player1, player2, gameRound };
+})();
+
+console.log(gameStart.player1.playerName);
