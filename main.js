5a436cdcc9f8b21b6da3fb7a04b7d37692e15b08
Ash Luchowa <ashwinluchowa@gmail.com>
2023-03-15 17:03:33 +0800

    Create gameOperator Module

--- main.js
+++ main.js
@@ -17,4 +17,37 @@ const gameStart = (() => {
   return { gameBoardContent, player1, player2, gameRound };
 })();
 
-console.log(gameStart.player1.playerName);
+//Game Operator Module
+const gameOperator = (() => {
+  //Click each square
+  const setSquareClick = (squareId, row, col) => {
+    const square = document.getElementById(squareId);
+    square.addEventListener("click", () => {
+      handleSquareClick(square, row, col);
+    });
+  };
+
+  //What happens for each square Function
+  const handleSquareClick = (squareElement, row, col) => {
+    if (squareElement.textContent === "") {
+      gameStart.gameBoardContent[row][col] = gameStart.player1.playerName;
+    }
+    console.log(gameStart.gameBoardContent);
+  };
+
+  //click events
+  const setSquareClickEvents = () => {
+    setSquareClick("square1", 0, 0);
+    setSquareClick("square2", 0, 1);
+    setSquareClick("square3", 0, 2);
+    setSquareClick("square4", 1, 0);
+    setSquareClick("square5", 1, 1);
+    setSquareClick("square6", 1, 2);
+    setSquareClick("square7", 2, 0);
+    setSquareClick("square8", 2, 1);
+    setSquareClick("square9", 2, 2);
+  };
+  setSquareClickEvents();
+
+  return { setSquareClick, handleSquareClick, setSquareClickEvents };
+})();
